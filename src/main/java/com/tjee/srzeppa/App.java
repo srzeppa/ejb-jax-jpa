7e4f160e28064acf73ef41103143db14452213cd
srzeppa <srzeppa@sigma.ug.edu.pl>
2015-12-16 08:47:36 +0100

    initialize project

---
+++ src/main/java/com/tjee/srzeppa/App.java
@@ -0,0 +1,13 @@
+package com.tjee.srzeppa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
