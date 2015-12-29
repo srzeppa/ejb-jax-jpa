e64b25e69e9302cc9182d0a0e9dc11b24df1a837
srzeppa <srzeppa@sigma.ug.edu.pl>
2015-12-29 17:49:43 +0100

    changed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 echo "************ UNDEPLOYING *******************"
-~/glassfish3/bin/asadmin undeploy jee-jax-jpa
+~/glassfish3/bin/asadmin undeploy ejb-jax-jpa
 echo "************ BUILDING **********************"
 mvn package
 echo "************ DEPLOYING *********************"
-~/glassfish3/bin/asadmin deploy target/jee-jax-jpa.war
+~/glassfish3/bin/asadmin deploy target/ejb-jax-jpa.war
