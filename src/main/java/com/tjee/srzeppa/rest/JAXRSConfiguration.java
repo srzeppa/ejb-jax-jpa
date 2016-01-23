ad8e5dc38ec7145416ff52aa693be081fe2ded26
srzeppa <srzeppa@sigma.ug.edu.pl>
2016-01-23 13:21:29 +0100

    deleted bad rest

--- src/main/java/com/tjee/srzeppa/rest/JAXRSConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.tjee.srzeppa.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest/")
-public class JAXRSConfiguration extends Application { }
